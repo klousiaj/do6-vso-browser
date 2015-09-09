286a5d57c136612c649aaddee9ad05eb3ddf3f1d
J.P. Klousia <klousiajp@gmail.com>
2015-09-08 21:43:54 -0400

    Add project endpoint to the server.

---
+++ server/api/project/project.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+
+// Get list of projects
+exports.index = function(req, res) {
+  res.json([]);
+};
--- server/api/workItems/workItems.controller.js
+++ server/api/workItems/workItems.controller.js
@@ -15,10 +15,9 @@ var _webApi = new webApiM.WebApi();
 
 var _witApi = _webApi.getQWorkItemTrackingApi(_url, [_bh, _vh]);
 
-/*
-* select a list of available queries.
-* 
-*/
+/**
+ * select the list of available queries
+ */
 exports.queries = function (req, res) {
   var site = req.site;
   _witApi.getQueries(site, 3, 2, false).done(function (response) {
@@ -31,11 +30,26 @@ exports.queries = function (req, res) {
     });
 };
 
-/* select a list of work items
-*
-*/
+/**
+ * select a list of work items.
+ */
 exports.workitem = function (req, res) {
   _witApi.getWorkItems('OjpApplicationMaintenance', 3, 2, false).done(function (response) {
+    res.json(response);
+  },
+    function (error) {
+      console.log('failure: ' + error);
+    }, function (err) {
+      console.log('in progress');
+    });
+}
+
+/**
+ * 
+ */
+exports.query = function (req, res) {
+  var queryId = req.id;
+  _witApi.queryById(queryId, 'OjpApplicationMaintenance').done(function (response) {
     res.json(response);
   },
     function (error) {
