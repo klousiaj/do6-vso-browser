1f4d4f37694a5597bb05c8396d82f7598ff6599b
J.P. Klousia <klousiajp@gmail.com>
2015-09-17 15:00:49 -0400

    Select and show sites as a dropdown.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('do6VsoBrowserApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    .controller('NavbarCtrl', function ($scope, $location, $http) {
+    $scope.menu = [];
+    $scope.selectedSite = 'OjpApplicationMaintenance';
+
+    $http.get('/api/projects/').success(function (sites) {
+      for (var ii = 0; ii < sites.length; ii++) {
+        console.log('site: ' + sites[ii].name);
+        var site = {
+          'title': sites[ii].name,
+          'link': '/queries/' + sites[ii].id
+        }
+        $scope.menu[ii] = site;
+      }
+    });
 
     $scope.isCollapsed = true;
 
