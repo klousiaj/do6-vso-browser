0c53ede8806776435906b91a2dedb1cb7568a240
J.P. Klousia <klousiajp@gmail.com>
2015-09-24 10:20:00 -0400

    comment out debug logging.

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('do6VsoBrowserApp', [
+var app = angular.module('do6VsoBrowserApp', [
   'angular-loading-bar',
   'ngCookies',
   'ngResource',
@@ -11,32 +11,34 @@ angular.module('do6VsoBrowserApp', [
   'ui.grid.exporter',
   'ui.select',
   'ui.router'
-])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
-    $locationProvider.html5Mode(true);
-  }).run(function ($rootScope) {
-    // these are here for debugging. Can/should be removed at some point.
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-      console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
-    });
-    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-      console.log('$stateChangeError - fired when an error occurs during transition.');
-      console.log(arguments);
-    });
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-      console.log('$stateChangeSuccess to ' + toState.name + '- fired once the state transition is complete.');
-    });
-    // $rootScope.$on('$viewContentLoading',function(event, viewConfig){
-    //   // runs on individual scopes, so putting it in "run" doesn't work.
-    //   console.log('$viewContentLoading - view begins loading - dom not rendered',viewConfig);
-    // });
-    $rootScope.$on('$viewContentLoaded', function (event) {
-      console.log('$viewContentLoaded - fired after dom rendered', event);
-    });
-    $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
-      console.log('$stateNotFound ' + unfoundState.to + '  - fired when a state cannot be found by its name.');
-      console.log(unfoundState, fromState, fromParams);
-    });
-  });
+]).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider
+    .otherwise('/');
+  $locationProvider.html5Mode(true);
+});
+  
+  
+// app.run(function ($rootScope) {
+//     // these are here for debugging. Can/should be removed at some point.
+//     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+//       console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
+//     });
+//     $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+//       console.log('$stateChangeError - fired when an error occurs during transition.');
+//       console.log(arguments);
+//     });
+//     $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+//       console.log('$stateChangeSuccess to ' + toState.name + '- fired once the state transition is complete.');
+//     });
+//     // $rootScope.$on('$viewContentLoading',function(event, viewConfig){
+//     //   // runs on individual scopes, so putting it in "run" doesn't work.
+//     //   console.log('$viewContentLoading - view begins loading - dom not rendered',viewConfig);
+//     // });
+//     $rootScope.$on('$viewContentLoaded', function (event) {
+//       console.log('$viewContentLoaded - fired after dom rendered', event);
+//     });
+//     $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+//       console.log('$stateNotFound ' + unfoundState.to + '  - fired when a state cannot be found by its name.');
+//       console.log(unfoundState, fromState, fromParams);
+//     });
+//   });
